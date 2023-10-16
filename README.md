dd97aeea510191ad79b7b6c47a5abc4974a5ae60
Sam Poder <sam.r.poder@gmail.com>
2023-10-15 23:09:36 -0700

    pfp

---
+++ README.md
@@ -0,0 +1,3 @@
+# 🎩 Profile pictures!
+
+Has a serverless function deployed on Vercel, being pinged by http://uptimerobot.com every 5 minutes as well as a [web dashboard](https://change-my-pfp.now.sh).
---
+++ pages/api/set-profile.js
@@ -0,0 +1,21 @@
+const axios = require("axios").default;
+import { WebClient } from "@slack/web-api";
+const S1 = require('s1db')
+const db = new S1(process.env.S1_TOKEN)
+
+export default async (req, res) => {
+  const client = new WebClient();
+  let frame = await db.get('frame')
+  frame += 1
+  if(frame > 15) frame = 0
+  let photo = `https://dj-pfp-hc.vercel.app/images/frame_${frame.toString().padStart(2, '0')}_delay-0.06s.jpg`
+  const image = await axios.get(photo, {
+    responseType: "arraybuffer",
+  });
+  const slackRequest = await client.users.setPhoto({
+    image: image.data,
+    token: process.env.SLACK_TOKEN,
+  });
+  await db.set('frame', frame)
+  res.redirect('https://dj-pfp-hc.vercel.app');
+};
